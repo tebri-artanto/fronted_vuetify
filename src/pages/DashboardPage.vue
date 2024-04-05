d51fa636a14fa2237b98b6e67fbb6cb607922057
tebri-artanto <tebriartanto@gmail.com>
2024-04-05 10:41:28 +0700

    changes to be committed

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -52,7 +52,7 @@
                   <div class="flex-auto p-4">
                     <div class="flex flex-wrap">
                       <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                        <h6 class="text-blueGray-400 uppercase font-bold text-2xl">{{ menuItem.title }}</h6>
+                        <h6 class="text-green-500 font-bold text-2xl">{{ menuItem.title }}</h6>
                       </div>
                       <div class="relative w-auto pl-4 flex-initial">
                         <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full" :class="menuItem.iconColor">
@@ -60,7 +60,7 @@
                         </div>
                       </div>
                     </div>
-                    <button class="text-blue-500 font-semibold mt-2" @click="$router.push(menuItem.route)">{{ menuItem.buttonText }}</button>
+                    <button class="text-black font-semibold mt-2" @click="$router.push(menuItem.route)">{{ menuItem.buttonText }}</button>
                   </div>
                 </div>
               </div>
